09649f81cbf0b4e5798f8264a6b5735b06c99175
armtronix <info@armtronix.net>
2017-10-23 11:11:46 +0530

    adding code

---
+++ Wifi-Single-Dimmer-Board/Arduino_Code/Atmega_Single_dimmerV0.2/Atmega_Single_dimmerV0.2.ino
@@ -0,0 +1,122 @@
+#include  <TimerOne.h>
+
+//Dimmer pin no.
+#define Dimmer 8 //Gpio 7       
+
+
+//manual switch
+//#define SWITCH_INPIN1 A0 //switch 1
+
+// Variables will change:
+//int buttonPushCounter = 0;   // counter for the number of button presses
+//int buttonState = 0;         // current state of the button
+//int lastButtonState = 0;     // previous state of the button
+
+int switch_status1;
+String serialReceived;
+String Dimmer_value;
+
+int freqStep = 75;//75*5 as prescalar is 16 for 80MHZ
+volatile int i = 0;
+int dimming = 115;
+volatile boolean zero_cross = 0;
+//int Dimmer = 6;
+int inc = 1;
+
+
+void setup() {
+
+  Serial.begin(115200);
+  Serial.println("WIFI SINGLE DIMMER V0.2");
+  pinMode(Dimmer, OUTPUT); //Dimmer output
+  //     pinMode(SWITCH_INPIN1, INPUT); //manual switch 1 input
+
+  attachInterrupt(0, zero_cross_detect, CHANGE);    // Attach an Interupt to Pin 2 (interupt 0) for Zero Cross Detection
+  Timer1.initialize(freqStep);                      // Initialize TimerOne library for the freq we need
+  Timer1.attachInterrupt(dim_check, freqStep);
+  //  attachInterrupt(digitalPinToInterrupt(Dimmer), Dimm, CHANGE);
+}
+
+//void Dimm()  //
+//{
+//   if(dimming == 0)
+//    {
+//    digitalWrite(Dimmer,LOW);
+//    }
+//  else if (dimming == 1)
+//  {
+//    digitalWrite(Dimmer,LOW);
+//
+//  }
+//}
+
+void zero_cross_detect() {
+  zero_cross = true;               // set the boolean to true to tell our dimming function that a zero cross has occured
+  i = 0;
+  digitalWrite(Dimmer, LOW);      // turn off TRIAC (and AC)
+}
+
+void dim_check() {
+  if (zero_cross == true) {
+    if (i >= dimming) {
+      digitalWrite(Dimmer, HIGH); // turn on light
+
+      i = 0; // reset time step counter
+      zero_cross = false; //reset zero cross detection
+    }
+    else {
+      i++; // increment time step counter
+    }
+  }
+}
+
+
+void loop() {
+
+  //   buttonState = digitalRead(SWITCH_INPIN1);
+  //   if (buttonState != lastButtonState) {
+  //
+  //    if (buttonState == HIGH) {
+  //      Serial.println("low");
+  //      Dimmer_value = "Dimmer:0";
+  //    } else {
+  //      Serial.println("high");
+  //      Dimmer_value = "Dimmer:99";
+  //    }
+  //    // Delay a little bit to avoid bouncing
+  //    delay(50);
+  //  }
+  //
+  //  lastButtonState = buttonState;
+
+
+
+
+  //        Serial.println(Serial.available());
+
+  if (Serial.available() > 0) {   // is a character available
+    serialReceived = Serial.readStringUntil('\n');
+    Serial.println(serialReceived);
+    if (serialReceived.substring(0, 7) == "Dimmer:")
+
+      Dimmer_value = serialReceived;
+
+  }
+
+
+
+  //Traic_out-----------------------------
+
+  if (Dimmer_value.substring(0, 7) == "Dimmer:")
+  {
+    dimming = 115 - Dimmer_value.substring(7, 9).toInt();
+    //      Serial.println(state_dimmer);
+    //         dimming =127-state_dimmer;
+    //         Serial.println(dimming);
+    delay(5);
+
+  }
+
+}
+
+
---
+++ Wifi-Single-Dimmer-Board/Arduino_Code/Wifi_Single_dimmerV0.2/Wifi_Single_dimmerV0.2.ino
@@ -0,0 +1,272 @@
+/*
+ *  This sketch is running a web server for configuring WiFI if can't connect or for controlling of one GPIO to switch a light/LED
+ *  Also it supports to change the state of the light via MQTT message and gives back the state after change.
+ *  The push button has to switch to ground. It has following functions: Normal press less than 1 sec but more than 50ms-> Switch light. Restart press: 3 sec -> Restart the module. Reset press: 20 sec -> Clear the settings in EEPROM
+ *  While a WiFi config is not set or can't connect:
+ *    http://server_ip will give a config page with 
+ *  While a WiFi config is set:
+ *    http://server_ip/gpio -> Will display the GIPIO state and a switch form for it
+ *    http://server_ip/gpio?state=0 -> Will change the GPIO directly and display the above aswell
+ *    http://server_ip/cleareeprom -> Will reset the WiFi setting and rest to configure mode as AP
+ *  server_ip is the IP address of the ESP8266 module, will be 
+ *  printed to Serial when the module is connected. (most likly it will be 192.168.4.1)
+ * To force AP config mode, press button 20 Secs!
+ *  For several snippets used, the credit goes to:
+ *  - https://github.com/esp8266
+ *  - https://github.com/chriscook8/esp-arduino-apboot
+ *  - https://github.com/knolleary/pubsubclient
+ *  - https://github.com/vicatcu/pubsubclient <- Currently this needs to be used instead of the origin
+ *  - https://gist.github.com/igrr/7f7e7973366fc01d6393
+ *  - http://www.esp8266.com/viewforum.php?f=25
+ *  - http://www.esp8266.com/viewtopic.php?f=29&t=2745
+ *  - And the whole Arduino and ESP8266 comunity
+ */
+
+#define DEBUG
+//#define WEBOTA
+//debug added for information, change this according your needs
+
+#ifdef DEBUG
+  #define Debug(x)    Serial.print(x)
+  #define Debugln(x)  Serial.println(x)
+  #define Debugf(...) Serial.printf(__VA_ARGS__)
+  #define Debugflush  Serial.flush
+#else
+  #define Debug(x)    {}
+  #define Debugln(x)  {}
+  #define Debugf(...) {}
+  #define Debugflush  {}
+#endif
+
+
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+//#include <EEPROM.h>
+#include <Ticker.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include "FS.h"
+
+extern "C" {
+  #include "user_interface.h" //Needed for the reset command
+}
+
+//***** Settings declare ********************************************************************************************************* 
+String hostName ="Armtronix"; //The MQTT ID -> MAC adress will be added to make it kind of unique
+int iotMode=0; //IOT mode: 0 = Web control, 1 = MQTT (No const since it can change during runtime)
+//select GPIO's
+#define OUTPIN_TRIAC 13 //output pin of Triac
+#define INPIN 0  // input pin (push button)
+#define OUTPIN_SSR 14  //output pin of SSR
+#define RESTARTDELAY 3 //minimal time in sec for button press to reset
+#define HUMANPRESSDELAY 50 // the delay in ms untill the press should be handled as a normal push by human. Button debounce. !!! Needs to be less than RESTARTDELAY & RESETDELAY!!!
+#define RESETDELAY 20 //Minimal time in sec for button press to reset all settings and boot to config mode
+#define AC_ZERO_CROSS  12   // input to Opto Triac pin   
+
+//##### Object instances ##### 
+MDNSResponder mdns;
+ESP8266WebServer server(80);
+WiFiClient wifiClient;
+PubSubClient mqttClient;
+Ticker btn_timer;
+Ticker otaTickLoop;
+
+//##### Flags ##### They are needed because the loop needs to continue and cant wait for long tasks!
+int rstNeed=0;   // Restart needed to apply new settings
+int toPub=0; // determine if state should be published.
+int configToClear=0; // determine if config should be cleared.
+int otaFlag=0;
+boolean inApMode=0;
+//##### Global vars ##### 
+int webtypeGlob;
+int otaCount=300; //imeout in sec for OTA mode
+int current; //Current state of the button
+
+int freqStep = 375;//75*5 as prescalar is 16 for 80MHZ
+volatile int i=0;
+volatile int dimming =0;  
+volatile boolean zero_cross=0;
+
+unsigned long count = 0; //Button press time counter
+String st; //WiFi Stations HTML list
+String state; //State of light
+char buf[40]; //For MQTT data recieve
+char* host; //The DNS hostname
+//To be read from Config file
+String esid="";
+String epass = "";
+String pubTopic;
+String subTopic;
+String mqttServer = "";
+const char* otaServerIndex = "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>";
+
+//-------------- void's -------------------------------------------------------------------------------------
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  // prepare GPIO2
+  pinMode(OUTPIN_TRIAC, OUTPUT);
+  pinMode(OUTPIN_SSR, OUTPUT);
+  pinMode(AC_ZERO_CROSS, INPUT);
+  pinMode(INPIN, INPUT_PULLUP);
+  //digitalWrite(OUTLED, HIGH);
+  btn_timer.attach(0.05, btn_handle);
+  Debugln("DEBUG: Entering loadConfig()");
+  if (!SPIFFS.begin()) {
+    Serial.println("Failed to mount file system");
+  }
+  
+  uint8_t mac[6];
+  WiFi.macAddress(mac);
+  hostName += "-";
+  hostName += macToStr(mac);
+  String hostTemp=hostName;
+  hostTemp.replace(":","-");
+  host = (char*) hostTemp.c_str();
+  loadConfig();
+  //loadConfigOld();
+  Debugln("DEBUG: loadConfig() passed");
+  
+  // Connect to WiFi network
+  Debugln("DEBUG: Entering initWiFi()");
+  initWiFi();
+  Debugln("DEBUG: initWiFi() passed");
+  Debug("iotMode:");
+  Debugln(iotMode);
+  Debug("webtypeGlob:");
+  Debugln(webtypeGlob);
+  Debug("otaFlag:");
+  Debugln(otaFlag);
+  Debugln("DEBUG: Starting the main loop");
+}
+
+void InitInterrupt(timercallback handler,int Step )
+{ 
+  timer1_disable();
+  timer1_isr_init();
+  timer1_attachInterrupt(handler);
+  timer1_enable(TIM_DIV16, TIM_EDGE, TIM_LOOP);
+  timer1_write(Step);//max 8388607 //75*5
+}
+
+void  ICACHE_RAM_ATTR do_on_delay()
+{
+  //digitalWrite(BUILTIN_LED, !digitalRead(BUILTIN_LED));
+  
+  if(zero_cross == true) 
+  {              
+    if(i>=dimming) 
+    {  
+                       
+     digitalWrite(OUTPIN_TRIAC, HIGH); // turn on light       
+     i=0;  // reset time step counter                         
+     zero_cross = false; //reset zero cross detection
+     delayMicroseconds(10);         // triac On propogation delay
+     digitalWrite(OUTPIN_TRIAC, LOW);    // triac Off
+    } 
+    else 
+    {
+     i++; // increment time step counter 
+     digitalWrite(OUTPIN_TRIAC, LOW);    // triac Off        
+                 
+    }                                
+  }    
+}
+
+void zero_crosss_int()  // function to be fired at the zero crossing to dim the light
+{
+  
+ zero_cross = true; 
+ i=0;
+  //InitInterrupt(do_something);
+ digitalWrite(OUTPIN_TRIAC, LOW);  
+ InitInterrupt(do_on_delay,freqStep);
+ 
+}
+
+void btn_handle()
+{
+  if(!digitalRead(INPIN)){
+    ++count; // one count is 50ms
+  } else {
+    if (count > 1 && count < HUMANPRESSDELAY/5) { //push between 50 ms and 1 sec      
+      Serial.print("button pressed "); 
+      Serial.print(count*0.05); 
+      Serial.println(" Sec."); 
+    
+      Serial.print("Light is ");
+      Serial.println(digitalRead(OUTPIN_SSR));
+      
+      Serial.print("Switching light to "); 
+      Serial.println(!digitalRead(OUTPIN_SSR));
+      digitalWrite(OUTPIN_SSR, !digitalRead(OUTPIN_SSR)); 
+      state = digitalRead(OUTPIN_SSR);
+      if(iotMode==1 && mqttClient.connected()){
+        toPub=1;        
+        Debugln("DEBUG: toPub set to 1");
+      }
+    } else if (count > (RESTARTDELAY/0.05) && count <= (RESETDELAY/0.05)){ //pressed 3 secs (60*0.05s)
+      Serial.print("button pressed "); 
+      Serial.print(count*0.05); 
+      Serial.println(" Sec. Restarting!"); 
+      setOtaFlag(!otaFlag);      
+      ESP.reset();
+    } else if (count > (RESETDELAY/0.05)){ //pressed 20 secs
+      Serial.print("button pressed "); 
+      Serial.print(count*0.05); 
+      Serial.println(" Sec."); 
+      Serial.println(" Clear settings and resetting!");       
+      configToClear=1;
+      }
+    count=0; //reset since we are at high
+  }
+}
+
+
+
+//-------------------------------- Main loop ---------------------------
+void loop() {
+  //Debugln("DEBUG: loop() begin");
+  if(configToClear==1){
+    //Debugln("DEBUG: loop() clear config flag set!");
+    clearConfig()? Serial.println("Config cleared!") : Serial.println("Config could not be cleared");
+    delay(1000);
+    ESP.reset();
+  }
+  //Debugln("DEBUG: config reset check passed");  
+  if (WiFi.status() == WL_CONNECTED && otaFlag){
+    if(otaCount<=1) {
+      Serial.println("OTA mode time out. Reset!"); 
+      setOtaFlag(0);
+      ESP.reset();
+      delay(100);
+    }
+    server.handleClient();
+    delay(1);
+  } else if (WiFi.status() == WL_CONNECTED || webtypeGlob == 1){
+    //Debugln("DEBUG: loop() wifi connected & webServer ");
+    if (iotMode==0 || webtypeGlob == 1){
+      //Debugln("DEBUG: loop() Web mode requesthandling ");
+      server.handleClient();
+      delay(1);
+    } else if (iotMode==1 && webtypeGlob != 1 && otaFlag !=1){
+          //Debugln("DEBUG: loop() MQTT mode requesthandling ");
+          if (!connectMQTT()){
+              delay(200);          
+          }                    
+          if (mqttClient.connected()){            
+              //Debugln("mqtt handler");
+              mqtt_handler();
+          } else {
+              Debugln("mqtt Not connected!");
+          }
+    }
+  } else{
+    Debugln("DEBUG: loop - WiFi not connected");  
+    delay(1000);
+    initWiFi(); //Try to connect again
+  }
+    //Debugln("DEBUG: loop() end");
+}
